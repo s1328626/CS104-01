cf634f961b0b564c1bf888040ed61ebbfafdd5dc
s1328626 <98922216+s1328626@users.noreply.github.com>
2022-02-02 20:29:57 -0500

    Verion 1 of hello_world first name only

---
+++ Hello_world.py
@@ -0,0 +1,10 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello, my name is Gina Elbanna")
+Hello, my name is Gina Elbanna
+print("Hello)
+      
+SyntaxError: unterminated string literal (detected at line 1)
+print("hello")
+      
+hello
