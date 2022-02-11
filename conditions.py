6090916521296b98527c7e9fc77dbdde4d48b255
s1328626 <98922216+s1328626@users.noreply.github.com>
2022-02-11 18:15:26 -0500

    conditions.py

---
+++ conditions.py
@@ -0,0 +1,9 @@
+a = 20000
+b = 20001
+
+if b > a:
+    print("b is greater than a")#does this if condition is true
+elif b==a:
+    print ("a and b are equal")#does this if condition is false
+else:
+    print("a is greater than b")
