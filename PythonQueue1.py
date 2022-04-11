ea8a08c48e9054f94cd54991a3c6e75e1a482660
s1328626 <98922216+s1328626@users.noreply.github.com>
2022-04-10 23:46:55 -0400

    Python Queue shell

---
+++ PythonQueue1.py
@@ -0,0 +1,36 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: C:\Users\G\AppData\Local\Programs\Python\Python310\PythonQueue.py ==
+simple counting loop with range
+enter name: John
+enter name: Jack
+enter name: Jake
+enter name: Jordan
+enter name: Jason
+enter name: Jackson
+enter name: Jared
+enter name: Jensen
+enter name: James
+enter name: Joan
+['John', 'Jack', 'Jake', 'Jordan', 'Jason', 'Jackson', 'Jared', 'Jensen', 'James', 'Joan']
+John
+['Jack', 'Jake', 'Jordan', 'Jason', 'Jackson', 'Jared', 'Jensen', 'James', 'Joan']
+Jack
+['Jake', 'Jordan', 'Jason', 'Jackson', 'Jared', 'Jensen', 'James', 'Joan']
+Jake
+['Jordan', 'Jason', 'Jackson', 'Jared', 'Jensen', 'James', 'Joan']
+Jordan
+['Jason', 'Jackson', 'Jared', 'Jensen', 'James', 'Joan']
+Jason
+['Jackson', 'Jared', 'Jensen', 'James', 'Joan']
+Jackson
+['Jared', 'Jensen', 'James', 'Joan']
+Jared
+['Jensen', 'James', 'Joan']
+Jensen
+['James', 'Joan']
+James
+['Joan']
+Joan
+[]
